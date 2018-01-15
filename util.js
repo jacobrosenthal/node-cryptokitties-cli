c2c478d6da982a33dcf435cce3730a0cb8f6cdde
Jacob Rosenthal <jakerosenthal@gmail.com>
2018-01-14 17:32:47 -0700

    psuedocode start

--- util.js
+++ util.js
@@ -1,6 +1,32 @@
 var cryptokittiesContrib = require("cryptokitties-contrib");
 var ck = new cryptokittiesContrib();
 const MAX = 20
+
+
+//TODO this request call is probably not written correctly 
+var getCurrentPrice = function() {
+  return request({
+      method: "GET",
+      uri: " https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD",
+      json: true,
+      orderBy,
+      orderDirection,
+      type,
+      status,
+      limit,
+      offset,
+      search
+    }).then(thing=>thing.USD);
+}
+
+var prettify = async function(argv, results) {
+  var price = await getCurrentPrice();
+
+  for (i=0; i<results.length; i++){
+    results[i] = prettyPrice(results[i], price)
+  }
+}
+
 var apiCall = async function (argv){
   var results =[];
   for(var i = 0; i<= Math.floor(argv.limit/MAX); i++) {
@@ -10,8 +36,9 @@ var apiCall = async function (argv){
     results = results.concat(set)
     var delay = await new Promise((resolve) => setTimeout(resolve, 2500))
   }
+
   if(argv.pretty){
-   results = results.map(prettyPrice);
+    results = prettify(argv, results)
   }
   console.log(JSON.stringify(results, null, 2))
   return results
@@ -55,15 +82,17 @@ var toFloat = function(val){
   return val / 1000000000000000000;
 }
 
-var prettyPrice = function(obj, index, array){
+
+var prettyPrice = function(obj, price){
+
   if(obj.start_price){
-    obj.start_price = toFloat(obj.start_price)
+    obj.start_price = toFloat(obj.start_price) * price
   }
   if(obj.end_price){
-    obj.end_price = toFloat(obj.end_price)
+    obj.end_price = toFloat(obj.end_price)  * price
   }
   if(obj.current_price){
-    obj.current_price = toFloat(obj.current_price)
+    obj.current_price = toFloat(obj.current_price)  * price
   }
   return obj;
 };
